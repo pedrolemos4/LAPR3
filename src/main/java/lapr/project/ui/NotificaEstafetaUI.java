5b9296702b1f82f529ff7dc922a4bd40a8cc7308
1190429 <1190429@isep.ipp.pt>
2021-01-06 23:31:07 +0000

    Revert "LAP3AP4-54 #close Notificar estafeta [Implementação] correção"
    This reverts commit 941a9176681356131c0a4da52e088ef54701f967.

--- src/main/java/lapr/project/ui/NotificaEstafetaUI.java
+++ src/main/java/lapr/project/ui/NotificaEstafetaUI.java
@@ -1,16 +1,15 @@
 package lapr.project.ui;
-/*
+
 import lapr.project.controller.NotificaEstafetaController;
 
 public class NotificaEstafetaUI {
     NotificaEstafetaController controller;
-
+/*
     public NotificaEstafetaUI(){
         if(controller.checkWellDockedScooter()){
             System.out.println("Scooter locked.\nEstimativa de tempo até estar completamente carregada: " + controller.calcEstimativa() +" horas.");
         }else{
             System.out.println("Scooter mal estacionada, tente de novo.");
         }
-    }
+    }*/
 }
-*/
