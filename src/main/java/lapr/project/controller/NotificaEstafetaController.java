04a9e5de8224fe55ddb9d2f73b7d843ff2c1433c
rodrigotavares222 <1191007@isep.ipp.pt>
2021-01-06 21:54:07 +0000

    LAP3AP-23 #close Notificar estafeta [Design][Implementação]

---
+++ src/main/java/lapr/project/controller/NotificaEstafetaController.java
@@ -0,0 +1,28 @@
+package lapr.project.controller;
+
+public class NotificaEstafetaController {
+
+    Scooter scoot;
+    EstacionamentoScooter estac;
+
+    public boolean checkWellDockedScooter(){
+        AplicacaoPOT app = AplicacaoPOT.getInstance();
+        SessaoUtilizador sessao = app.getSessaoAtual();
+        String email = sessao.getEmail();
+        Estafeta est = EstafetaDB.getEstafetaByEmail(email);
+        Entrega ent = EntregaDB.getActiveEntrega(est);
+        scoot = ent.getScooter();
+
+        if(EstacionamentoScooterDB.getActiveParking(scoot) != null){
+            estac = EstacionamentoScooterDB.getActiveParking(scoot);
+            return true;
+        }
+        return false;
+    }
+
+    public double calcEstimativa(){
+        double bateriaAtual = scoot.percentagemBateria;
+        double bateriaNecessaria = 100 - bateriaAtual;
+        return bateriaNecessaria; //DEFINIR CONSTANTE
+    }
+}
