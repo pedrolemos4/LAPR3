76ab04f5675a28bf059022c9b34467c335a4a67c
1190769 <1190769@isep.ipp.pt>
2021-01-08 17:02:13 +0000

    LAP3AP4-108 #close Testes Unitários

---
+++ src/test/java/lapr/project/data/EstafetaDBTest.java
@@ -0,0 +1,51 @@
+package lapr.project.data;
+
+import lapr.project.model.EstadoEstafeta;
+import lapr.project.model.Estafeta;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EstafetaDBTest {
+
+    @Test
+    void getLstEstafetas() {
+        System.out.println("getLstEstafetas");
+        List<Estafeta> expResult = new ArrayList<>();
+        Estafeta instance = new Estafeta(55, "ss", "dd", 58, 55, "sd", new EstadoEstafeta());
+        expResult.add(instance);
+
+        List<Estafeta> result = expResult;
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    void novoEstafeta() {
+    }
+
+    @Test
+    void registaEstafeta() {
+    }
+
+    @Test
+    void validaEstafeta() {
+        System.out.println("validaEstafeta");
+        EstafetaDB edb = new EstafetaDB();
+        Estafeta instance = new Estafeta(55, "ss", "dd", 58, 55, "sd", new EstadoEstafeta());
+        boolean expResult = true;
+
+        boolean result = edb.validaEstafeta(instance);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    void addEstafeta() {
+    }
+
+    @Test
+    void getEstafetaByEmail() {
+    }
+}
