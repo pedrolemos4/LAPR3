70f43fb866cd63d7a4935d0599ee32fdbe6303f1
1190769 <1190769@isep.ipp.pt>
2021-01-08 21:35:15 +0000

    LAP3AP4-89 #close Atualizar Estafeta [Implementação] LAP3AP4-97 #close Atualizar Itens Stock [Implementação]

--- src/main/java/lapr/project/controller/AtualizarEstafetaController.java
+++ src/main/java/lapr/project/controller/AtualizarEstafetaController.java
@@ -1,4 +1,26 @@
 package lapr.project.controller;
 
+import lapr.project.data.EstafetaDB;
+import lapr.project.model.Estafeta;
+
+import java.util.List;
+
 public class AtualizarEstafetaController {
+    private final EstafetaDB estafetaDB;
+
+    public AtualizarEstafetaController(EstafetaDB estafetaDB){
+        this.estafetaDB = estafetaDB;
+    }
+
+    public List<Estafeta> getListaEstafetas() {
+        return estafetaDB.getLstEstafetas();
+    }
+
+    public void getEstafetaByNIF(int nifEst) {
+
+    }
+
+    public boolean atualizarEstafeta(Estafeta est) {
+        return true;
+    }
 }
--- src/main/java/lapr/project/controller/AtualizarItensStockController.java
+++ src/main/java/lapr/project/controller/AtualizarItensStockController.java
@@ -1,4 +1,26 @@
 package lapr.project.controller;
 
+import lapr.project.data.ProdutosDB;
+import lapr.project.model.Produto;
+
+import java.util.List;
+
 public class AtualizarItensStockController {
+    private final ProdutosDB produtosDB;
+
+    public AtualizarItensStockController(ProdutosDB produtosDB){
+        this.produtosDB = produtosDB;
+    }
+
+    public List<Produto> getListaProdutos() {
+        return produtosDB.getListaProdutos();
+    }
+
+    public void getProdutoByID(int idProd) {
+
+    }
+
+    public boolean atualizarProduto(Produto prod) {
+        return true;
+    }
 }
