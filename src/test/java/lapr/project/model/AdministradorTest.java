f13f61b04e1c172442a666335d3af94e8201952a
rodrigotavares222 <1191007@isep.ipp.pt>
2021-01-08 00:21:29 +0000

    LAP3AP4-108 #close Testes Unitários

--- src/test/java/lapr/project/model/AdministradorTest.java
+++ src/test/java/lapr/project/model/AdministradorTest.java
@@ -1,6 +1,15 @@
 package lapr.project.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdministradorTest extends UtilizadorTest {
+
+    @Test
+    public void AdministradorConstructorTest(){
+        Administrador adminTest = new Administrador();
+
+        assertEquals(adminTest,adminTest);
+    }
 }
