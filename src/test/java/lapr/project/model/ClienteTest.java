c3c2fffa7dbb0de12ca1f631e8aadd1c44aafd75
1190785 <1190785@isep.ipp.pt>
2021-01-08 22:29:05 +0000

    LAP3AP4-108 #close Testes Unitários

--- src/test/java/lapr/project/model/ClienteTest.java
+++ src/test/java/lapr/project/model/ClienteTest.java
@@ -4,53 +4,71 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ClienteTest extends UtilizadorTest{
+class ClienteTest extends UtilizadorTest {
 
     @Test
-    public void ClienteEmptyConstructorTest(){
+    public void ClienteEmptyConstructorTest() {
         Cliente clienteTest = new Cliente();
 
-        assertEquals(clienteTest,clienteTest);
+        assertEquals(clienteTest, clienteTest);
     }
 
     @Test
-    public void ClienteConstructorTest(){
-        Cliente novoCliente = new Cliente(3,new Cartao(),3,"teste","@",123,"teste");
-
-        assertEquals(novoCliente,novoCliente);
+    public void ClienteConstructorTest() {
+        Cliente novoCliente = new Cliente(3, new Cartao(), new Endereco(), 3, "teste", "@", 123, "teste");
+        assertEquals(novoCliente, novoCliente);
     }
 
     @Test
     void getCreditos() {
         Cartao cartao = new Cartao();
-        Cliente novoCliente = new Cliente(3,cartao,3,"teste","@",123,"teste");
-        assertEquals(3,novoCliente.getCreditos());
+        Endereco endereco = new Endereco();
+        Cliente novoCliente = new Cliente(3, cartao, endereco, 3, "teste", "@", 123, "teste");
+        assertEquals(3, novoCliente.getCreditos());
     }
 
     @Test
     void setCreditos() {
         Cartao cartao = new Cartao();
-        Cliente novoCliente = new Cliente(3,cartao,3,"teste","@",123,"teste");
+        Endereco endereco = new Endereco();
+        Cliente novoCliente = new Cliente(3, cartao, endereco, 3, "teste", "@", 123, "teste");
         novoCliente.setCreditos(2);
-        assertEquals(2,novoCliente.getCreditos());
+        assertEquals(2, novoCliente.getCreditos());
     }
 
     @Test
     void getCartaoCredito() {
         Cartao cartao = new Cartao();
-        Cliente novoCliente = new Cliente(3,cartao,3,"teste","@",123,"teste");
-
-        assertEquals(cartao,novoCliente.getCartaoCredito());
+        Endereco endereco = new Endereco();
+        Cliente novoCliente = new Cliente(3, cartao, endereco, 3, "teste", "@", 123, "teste");
+        assertEquals(cartao, novoCliente.getCartaoCredito());
     }
 
     @Test
     void setCartaoCredito() {
         Cartao cartao = new Cartao();
-        Cliente novoCliente = new Cliente(3,cartao,3,"teste","@",123,"teste");
-
-        Cartao novoCartao = new Cartao(123,"123",123);
+        Endereco endereco = new Endereco();
+        Cliente novoCliente = new Cliente(3, cartao, endereco, 3, "teste", "@", 123, "teste");
+        Cartao novoCartao = new Cartao(123, "123", 123);
         novoCliente.setCartaoCredito(novoCartao);
+        assertEquals(novoCartao, novoCliente.getCartaoCredito());
+    }
 
-        assertEquals(novoCartao,novoCliente.getCartaoCredito());
+    @Test
+    void getEndereco() {
+        Cartao cartao = new Cartao();
+        Endereco endereco = new Endereco();
+        Cliente novoCliente = new Cliente(3, cartao, endereco, 3, "teste", "@", 123, "teste");
+        assertEquals(endereco, novoCliente.getEndereco());
+    }
+
+    @Test
+    void setEndereco() {
+        Cartao cartao = new Cartao();
+        Endereco endereco = new Endereco();
+        Cliente novoCliente = new Cliente(3, cartao, endereco, 3, "teste", "@", 123, "teste");
+        Endereco novoEndereco = new Endereco("nada", 5764, 3242, 21312);
+        novoCliente.setEndereco(novoEndereco);
+        assertEquals(novoEndereco, novoCliente.getEndereco());
     }
 }
