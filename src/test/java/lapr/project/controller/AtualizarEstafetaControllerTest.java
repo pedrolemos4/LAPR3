bb3886b578450e3ae5d537d682837fe16339abbd
1190769 <1190769@isep.ipp.pt>
2021-01-10 21:31:24 +0000

    LAP3AP4-108 #close Testes Unitários

---
+++ src/test/java/lapr/project/controller/AtualizarEstafetaControllerTest.java
@@ -0,0 +1,20 @@
+package lapr.project.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AtualizarEstafetaControllerTest {
+
+    @Test
+    void getListaEstafetas() {
+    }
+
+    @Test
+    void getEstafetaByEmail() {
+    }
+
+    @Test
+    void atualizarEstafeta() {
+    }
+}
