07586e7945d929b671f3379a46d015ad1da51180
1190769 <1190769@isep.ipp.pt>
2021-01-11 22:08:55 +0000

    LAP3AP4-121 #close Otimizar Código

--- src/test/java/lapr/project/controller/RegistarEstafetaControllerTest.java
+++ src/test/java/lapr/project/controller/RegistarEstafetaControllerTest.java
@@ -1,26 +1,17 @@
 package lapr.project.controller;
 
-import lapr.project.model.Estafeta;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RegistarEstafetaControllerTest {
-    RegistarEstafetaController controller;
 
     @Test
     void getListaEstafetas() {
-        List<Estafeta> le = new ArrayList<>();
-        assertEquals(le, controller.getListaEstafetas());
     }
 
     @Test
     void novoEstafeta() {
-        Estafeta est = new Estafeta(1,"estafeta", "est@email.com", 1, 50, "password",1);
-        assertEquals(est, controller.novoEstafeta(1,"estafeta", "est@email.com", 1, 50, "password"));
     }
 
     @Test
