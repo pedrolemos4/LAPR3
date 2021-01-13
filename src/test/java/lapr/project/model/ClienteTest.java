fbc2c4c919b96f14f5bb2e9244cf4de365f36771
1190429 <1190429@isep.ipp.pt>
2021-01-13 13:55:32 +0000

    LAP3AP4-121 #close Otimizar Código

--- src/test/java/lapr/project/model/ClienteTest.java
+++ src/test/java/lapr/project/model/ClienteTest.java
@@ -26,61 +26,68 @@ class ClienteTest extends UtilizadorTest {
     }
 
     @Test
-    void getClienteNIF() {
+    public void getClienteNIF() {
         Cliente novoCliente = new Cliente(56, 123, "não sei", 65746);
         assertEquals(56, novoCliente.getClienteNIF());
     }
 
     @Test
-    void setClienteNIF() {
+    public void setClienteNIF() {
         Cliente novoCliente = new Cliente(123, 56, "não sei", 65746);
         novoCliente.setClienteNIF(78);
         assertEquals(78, novoCliente.getClienteNIF());
     }
 
     @Test
-    void getCreditos() {
+    public void getCreditos() {
         Cliente novoCliente = new Cliente(123, 56, "não sei", 65746);
         assertEquals(56, novoCliente.getCreditos());
     }
 
     @Test
-    void setCreditos() {
+    public void setCreditos() {
         Cliente novoCliente = new Cliente(123, 56, "não sei", 65746);
         novoCliente.setCreditos(2.0);
         assertEquals(2, novoCliente.getCreditos());
     }
 
     @Test
-    void getNumCartaoCredito() {
+    public void getNumCartaoCredito() {
         Cliente novoCliente = new Cliente(123, 56, "não sei", 65746);
         assertEquals(65746, novoCliente.getNumCartaoCredito());
     }
 
     @Test
-    void setNumCartaoCredito() {
+    public void setNumCartaoCredito() {
         Cliente novoCliente = new Cliente(123, 56, "não sei", 65746);
         novoCliente.setNumCartaoCredito(3422);
         assertEquals(3422, novoCliente.getNumCartaoCredito());
     }
 
     @Test
-    void getEnderecoMorada() {
+    public void getEnderecoMorada() {
         Cliente novoCliente = new Cliente(123, 56, "não sei", 65746);
         assertEquals("não sei", novoCliente.getEnderecoMorada());
     }
 
     @Test
-    void setEnderecoMorada() {
+    public void setEnderecoMorada() {
         Cliente novoCliente = new Cliente(123, 56, "não sei", 65746);
         novoCliente.setEnderecoMorada("sei");
         assertEquals("sei", novoCliente.getEnderecoMorada());
     }
 
+    /**
+     * Test of toString method, of class Cliente.
+     */
     @Test
-    public void testToString(){
+    public void testToString() {
+        System.out.println("toString");
         Cliente instance = new Cliente(123, 56, "não sei", 65746);
-        String expResult = instance.toString();
-        assertEquals(expResult,instance.toString());
+        String expResult = "Cliente{nif=123, creditos=56.0, enderecomorada=não sei, numCartaoCredito65746}";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+
     }
+    
 }
