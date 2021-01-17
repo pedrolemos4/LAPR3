f1802407a23ce2ab61792ed0d869cbebe4afeaf2
1190785 <1190785@isep.ipp.pt>
2021-01-17 23:08:56 +0000

    LAP3AP4-121 #close Otimizar Código

--- src/main/java/lapr/project/ui/InserirEstacionamentosUI.java
+++ src/main/java/lapr/project/ui/InserirEstacionamentosUI.java
@@ -38,48 +38,53 @@ public class InserirEstacionamentosUI {
         System.out.println("Introduza o NIF do farmácia:");
         int nif = LER.nextInt();
         Farmacia farmacia = controller.getFarmaciaByNIF(nif);
-        List<Parque> lstParques = controller.getListaParquesByFarmaciaNif(farmacia.getNIF());
-        for (Parque p : lstParques) {
-            System.out.println(p);
-        }
-        System.out.println("Introduza o id do parque para o qual pretende adicionar os estacionamentos:");
-        int idParque = LER.nextInt();
-        List<Estacionamento> lstEstacs = controller.getListaEstacionamentosByFarmaciaNifParqueId(nif, idParque);
-        if (lstEstacs == null) {
-            int numMax = controller.getNumMaxByFarmaciaNifParqueId(nif, idParque);
-            for (int i = 0; i < numMax; i++) {
-                System.out.println("Introduza a disponibilidade do carregador do estacionamento " + i);
-                System.out.println("Indisponível - 0");
-                System.out.println("Disponível - 1");
-                int carregador = LER.nextInt();
-                while (carregador != 0 && carregador != 1) {
-                    System.out.println("Número inválido, introduzia novamente!");
-                    carregador = LER.nextInt();
+        if (farmacia != null) {
+            List<Parque> lstParques = controller.getListaParquesByFarmaciaNif(farmacia.getNIF());
+            for (Parque p : lstParques) {
+                System.out.println(p);
+            }
+            System.out.println("Introduza o id do parque para o qual pretende adicionar os estacionamentos:");
+            int idParque = LER.nextInt();
+            List<Estacionamento> lstEstacs = controller.getListaEstacionamentosByFarmaciaNifParqueId(nif, idParque);
+            if (lstEstacs == null) {
+                int numMax = controller.getNumMaxByFarmaciaNifParqueId(nif, idParque);
+                for (int i = 0; i < numMax; i++) {
+                    System.out.println("Introduza a disponibilidade do carregador do estacionamento " + i);
+                    System.out.println("Indisponível - 0");
+                    System.out.println("Disponível - 1");
+                    int carregador = LER.nextInt();
+                    while (carregador != 0 && carregador != 1) {
+                        System.out.println("Número inválido, introduzia novamente!");
+                        carregador = LER.nextInt();
+                    }
+                    int numLote = i + 1;
+                    Estacionamento estac = controller.novoEstacionamento(numLote, carregador, idParque);
+                    lestac.add(estac);
+                    System.out.println("--Estacionamento Criado--");
                 }
-                int numLote = i + 1;
-                Estacionamento estac = controller.novoEstacionamento(numLote, carregador, idParque);
-                lestac.add(estac);
-                System.out.println("--Estacionamento Criado--");
-            }
-            for (Estacionamento est : lestac) {
-                System.out.println(est);
-            }
-            System.out.println("Deseja registar os Estacionamentos criados? (S/N)");
-            String confirm = LER.next();
+                for (Estacionamento est : lestac) {
+                    System.out.println(est);
+                }
+                System.out.println("Deseja registar os Estacionamentos criados? (S/N)");
+                String confirm = LER.next();
 
-            if (confirm.equalsIgnoreCase("S") || confirm.equalsIgnoreCase("SIM")) {
-                controller.registaEstacionamentos(lestac);
-                System.out.println("\n\nEstacionamentos registados com sucesso!");
+                if (confirm.equalsIgnoreCase("S") || confirm.equalsIgnoreCase("SIM")) {
+                    controller.registaEstacionamentos(lestac);
+                    System.out.println("\n\nEstacionamentos registados com sucesso!");
+                } else {
+                    System.out.println("\n\nRegisto de Estacionamentos cancelado!");
+                }
+
             } else {
-                System.out.println("\n\nRegisto de Estacionamentos cancelado!");
+                System.out.println("Registo de Estacionamentos cancelado!");
+                System.out.println("O parque selecionado já tem estacionamentos registados!");
+                for (Estacionamento e : lstEstacs) {
+                    System.out.println(e);
+                }
             }
-
         } else {
             System.out.println("Registo de Estacionamentos cancelado!");
-            System.out.println("O parque selecionado já tem estacionamentos registados!");
-            for (Estacionamento e : lstEstacs) {
-                System.out.println(e);
-            }
+            System.out.println("NIF da farmácia introduzido é inválido!");
         }
     }
 }
