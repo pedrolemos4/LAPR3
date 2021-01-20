ae32a41094a428e4a87bb7604f7ae572686a402b
1190769 <1190769@isep.ipp.pt>
2021-01-20 10:22:39 +0000

    LAP3AP4-233 #close Testes Unitários

--- src/test/java/lapr/project/model/LerFicheiroTest.java
+++ src/test/java/lapr/project/model/LerFicheiroTest.java
@@ -7,8 +7,14 @@ package lapr.project.model;
 
 import java.sql.SQLException;
 import java.text.ParseException;
+import java.util.List;
+
+import lapr.project.data.FarmaciaDB;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -17,10 +23,21 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LerFicheiroTest {
 
     private static LerFicheiro instance;
+    private FarmaciaDB farmaciaMock;
+    private Farmacia farm;
+
+    @BeforeEach
+    public void setUp() throws SQLException {
+        farmaciaMock = mock(FarmaciaDB.class);
+        farm = new Farmacia(123456789, "email", "rua1");
+        when(farmaciaMock.addFarmacia(farm)).thenReturn(true);
+    }
 
     public LerFicheiroTest() throws ParseException, SQLException {
         instance = new LerFicheiro();
-        instance.read("docs/Dados_de_Leitura/farmacias.csv");
+        LerFicheiro expResult = instance;
+        LerFicheiro result = expResult;
+        assertEquals(result, expResult);
     }
 
     /**
@@ -31,8 +48,26 @@ public class LerFicheiroTest {
     @Test
     public void testRead() throws Exception {
         System.out.println("read");
+        farm = new Farmacia(123456789, "email", "rua1");
         String nameFile = "docs/Dados_de_Leitura/farmacias.csv";
         instance.read(nameFile);
+        Farmacia result = farmaciaMock.getFarmaciaByNIF(123456789);
+        assertEquals(null, result);
+    }
+
+    /**
+     * Test of read method, of class LerFicheiro.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testRead1() throws Exception {
+        System.out.println("read1");
+        List<Farmacia> lf = farmaciaMock.getLstFarmacias();
+        String nameFile = "docs/Dados_de_Leitura/farmacias.csv";
+        instance.read(nameFile);
+        List<Farmacia> lf2 = farmaciaMock.getLstFarmacias();
+        assertEquals(lf, lf2);
     }
 
 }
