ef65dacf6854e626249bf8b023a4158374893ac5
rodrigotavares222 <1191007@isep.ipp.pt>
2021-01-21 12:02:47 +0000

    LAP3AP4-198 #close [Implementação] Carregamento de veículos simultaneamente

--- src/main/java/lapr/project/parking/main_2_0.c
+++ src/main/java/lapr/project/parking/main_2_0.c
@@ -3,36 +3,37 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "calc_estimativa.h"
+//#include "calc_estimativa.h"
 #include "read_file.h"
 
 struct Estacionamento arrayEstacionamentos[100];
+int current = 0;
 
 int main(void){
-	int run = 0;
-	while(run == 0){
+	//int run = 0;
+	//while(run == 0){
 		DIR *folder;
 	
 		struct dirent *entry;
 	
 		folder = opendir("./estacionamento");
 	
-		int current = 0;
+		current = 0;
 	
 		while((entry = readdir(folder))!= NULL){
 			char *extensao = strrchr(entry->d_name, '.');
 			if(strcmp(extensao,".data") == 0){
 				printf("nextreadfile\n");
-				readFile(entry->d_name,current);
+				readFile(entry->d_name);
 			}
 		}
-		sleep(15);
-	}
+		//sleep(15);
+	//}
 
 	return 0;
 }
 
-int readFile(char* nomeFicheiro,int current){
+int readFile(char* nomeFicheiro){
 	char flag[] = ".flag";
 	char nomeFicheiroFlag[50];
 	
@@ -49,14 +50,14 @@ int readFile(char* nomeFicheiro,int current){
 	while((entry = readdir(folder))!= NULL){
 		if(strcmp(entry->d_name,nomeFicheiroFlag) == 0){
 			printf("foundflag,tratamento\n");
-			tratamentoFile(nomeFicheiro,nomeFicheiroFlag,current);
+			tratamentoFile(nomeFicheiro,nomeFicheiroFlag);
 		}
 	}
 	
 	return 0;
 }
 
-int tratamentoFile(char* nomeFicheiro, char* nomeFicheiroFlag, int current){
+int tratamentoFile(char* nomeFicheiro, char* nomeFicheiroFlag){
 	int percentagem;
 	int capacidade;
 	int idParque;
@@ -84,30 +85,70 @@ int tratamentoFile(char* nomeFicheiro, char* nomeFicheiroFlag, int current){
 		
 	int corrente = 0.1 * capacidade;
 	
-	struct Estacionamento mesmoParque[current];
-	
-	struct Estacionamento * pointer = mesmoParque;
+	struct Estacionamento samePark [current];
 	
 	int i;
 	int j = 0;
+	int correnteTotal = corrente;
 	for(i = 0; i < current;i++){
 		if(arrayEstacionamentos[i].idParque == idParque){
-			*(pointer + j) = arrayEstacionamentos[i];
+			samePark[j].dirFicheiro = arrayEstacionamentos[i].dirFicheiro;
+			correnteTotal = correnteTotal + (arrayEstacionamentos[i].capacidadeBateria * 0.1);
 			j++;
 		}
 	}
+	printf("Passo a passo imroum: %d\n", correnteTotal);
 	
-	printf("%d\n%d\n%s\n",percentagem, capacidade, content);
+	if(correnteTotal > capacidadeParque){
+		capacidadeParque = capacidadeParque/(j+1);
+		int k;
+		for(i = 0; i < j;i++){
+			for(k = 0; k < current; k++){
+				
+				if(strcmp(samePark[i].dirFicheiro,arrayEstacionamentos[k].dirFicheiro) == 0){
+					estimateFile(arrayEstacionamentos[k].dirFicheiro,arrayEstacionamentos[k].percentagem,arrayEstacionamentos[k].capacidadeBateria,arrayEstacionamentos[k].idParque,arrayEstacionamentos[k].estimativa,capacidadeParque,arrayEstacionamentos[k].content);
+				}
+				
+			}
+		}
+	}
+	
+	estimateFile(dirlock,percentagem,capacidade,idParque,estimativa,capacidadeParque, content);
+	
+	printf("Percentage: %d\nCapacidade: %d\nContent: %s\n",percentagem, capacidade, content);
 		
 	fclose(lockFilePtr);
 		
-	estimativa = calc_estimativa(percentagem, capacidade, corrente);
+	//estimativa = calc_estimativa(percentagem, capacidade, corrente);
 	
 	arrayEstacionamentos[current].percentagem = percentagem;
+	printf("bateria: %d\n",arrayEstacionamentos[current].percentagem);
 	arrayEstacionamentos[current].capacidadeBateria = capacidade;
 	arrayEstacionamentos[current].idParque = idParque;
 	arrayEstacionamentos[current].estimativa = estimativa;
+	arrayEstacionamentos[current].dirFicheiro = dirlock;
+	//arrayEstacionamentos[current].dirEstimate = direstimate;
+	arrayEstacionamentos[current].content = content;
+	printf("current:%d\n",current);
+	current++;
+	
 
+	return 0;
+}
+
+int estimateFile(char* dirlock, int percentagem, int capacidade, int idParque, int estimativa, int corrente, char * content){
+	
+	estimativa = calc_estimativa(percentagem, capacidade, corrente);
+	
+	int i;
+	for(i = 0; i < current; i++){
+		if(dirlock == arrayEstacionamentos[i].dirFicheiro){
+			arrayEstacionamentos[i].estimativa = estimativa;
+		}
+	}
+	
+	//criar ficheiro estimate
+	
 	return 0;
 }
 
